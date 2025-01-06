3b25fcb20e8a806afd6533a5ae1871c6b20808d1
sjh4616 <41767000+sjh4616@users.noreply.github.com>
2025-01-06 14:50:03 +0900

    Update install-docker.sh

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -5,7 +5,6 @@ apt-get update -y && apt-get install -y apt-transport-https \
        software-properties-common
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
 add-apt-repository \
        "deb [arch=amd64] https://download.docker.com/linux/debian \
        $(lsb_release -cs) stable"
