2804f9ad3df0b3ceaacf21fcd2fa4c4eaac9bb85
sjh4616 <41767000+sjh4616@users.noreply.github.com>
2025-01-06 13:33:50 +0900

    Update install-docker.sh

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y apt-transport-https \
+apt-get update -y && apt-get install -y apt-transport-https \
        ca-certificates curl gnupg2 \
        software-properties-common
 
@@ -10,4 +10,4 @@ add-apt-repository \
        "deb [arch=amd64] https://download.docker.com/linux/debian \
        $(lsb_release -cs) stable"
 
-apt-get update && apt-get install -y docker-ce-cli
+apt-get update -y && apt-get install -y docker-ce-cli
